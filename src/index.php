57bea7e4fae8329d09d472df966b25b0f321e0b6
AlexCadigan <cadiganalexander@gmail.com>
2018-02-12 13:43:55 -0500

    Setting up website

---
+++ src/index.php
@@ -0,0 +1,8 @@
+<head>
+	<title> Kalamazoo Loaves and Fishes </title>
+	<link rel = "icon" href = "img/KLFLogo.jpg">
+	<link rel = "stylesheet" href = "index.css">
+</head>
+<body>
+	Kalamazoo Loaves and Fishes - Mobile Food Initiative
+</body>
