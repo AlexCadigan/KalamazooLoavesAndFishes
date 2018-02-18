f6f0c77578155492a983e3c747e4b7a766218314
AlexCadigan <cadiganalexander@gmail.com>
2018-02-18 11:51:27 -0500

    Established database connection

--- src/lib/config.php
+++ src/lib/config.php
@@ -19,3 +19,5 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 -->
+<?php
+mysqli_connect("localhost", "id4726014_kalamazooloavesandfishes", "COMP-482", "id4726014_kalamazooloavesandfishes"); ?>
