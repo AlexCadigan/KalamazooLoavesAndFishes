d90f0f1ec03776c0c3d2bcd63f4001aee5d0fca3
AlexCadigan <cadiganalexander@gmail.com>
2018-03-04 00:12:04 -0500

    Check ins are working

--- src/DisplayClientInfo.php
+++ src/DisplayClientInfo.php
@@ -21,40 +21,82 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 -->
+<html>
+    <head>
+        <title> KLF - MFI </title>
+        <link rel = "icon" href = "img/KLFLogo.jpg">
+        <style>
+            .page {
+                text-align: center;
+            }
+            .instructions {
+                text-align: center;
+                font-size: 30px;
+            }
+            #table {
+                margin-left: auto;
+                margin-right: auto;
+            }
+        </style>
+    </head>
+    <body>
+        <div class = "instructions">
+            Is the following information about yourself and your household correct?
+        </div> <br>
 <?php
 require "lib/config.php";
 $sql_checkin = "SELECT * FROM People WHERE (FirstName='" . $_POST['fname'] . "') AND (LastName='" . $_POST['lname'] . "') AND (DateofBirth='" . $_POST['DOB'] . "') LIMIT 1";
 $result_checkin = mysqli_query($connection, $sql_checkin);
 // if the results are non empty
 if ($result_checkin->num_rows > 0) {
-  // fetch householdID
-  $result = $result_checkin -> fetch_assoc();
-  $householdID = $result['HouseholdID'];
-  // display household information
-  $household_query = mysqli_query($connection, "SELECT Street, City, ZIP, Size FROM Households WHERE ID='" . $householdID . "' LIMIT 1");
-  while ($hh = $household_query->fetch_assoc()) {
-    echo "Household Address: " . $hh['Street'] . " " . $hh['City'] . " " . $hh['ZIP'] . "<br>";
-    echo "Household Size: " . $hh['Size'] . "<br><br>";
-  }
-  $members_query = mysqli_query($connection, "SELECT FirstName, LastName, DateOfBirth FROM People WHERE HouseholdID='" . $householdID . "'");
-  // display table of household members
-  echo "<table>";
-  echo "<tr><th>First Name</th><th>Last Name</th><th>Date of Birth</th></tr>";
-  while($row = $members_query->fetch_assoc()) {
-    echo "<tr><td>" . $row['FirstName'] . "</td><td>" . $row['LastName'] . "</td><td>" . $row['DateOfBirth'] . "</tr>";
-  }
-  echo "</table>";
+    // fetch householdID
+    $result = $result_checkin -> fetch_assoc();
+    $householdID = $result['HouseholdID'];
+    // display household information
+    $household_query = mysqli_query($connection, "SELECT Street, City, ZIP, Size FROM Households WHERE ID='" . $householdID . "' LIMIT 1");
+    while ($hh = $household_query->fetch_assoc()) {
+        echo "<form class = 'page' method = 'POST' action = 'lib/InsertCheckIn.php'> 
+            <fieldset>
+                <legend> Household Address: </legend>
+                <input type = 'text' name = 'street' value = '" . $hh['Street'] . "' readonly>
+                <input type = 'text' name = 'city' value = '" . $hh['City'] . "' readonly>
+                <input type = 'text' name = 'ZIP' value = '" . $hh['ZIP'] . "' readonly>
+            </fieldset> <br>";
+    }
+    $members_query = mysqli_query($connection, "SELECT FirstName, LastName, DateOfBirth FROM People WHERE HouseholdID='" . $householdID . "'");
+    // display table of household members
+    echo "<fieldset>
+        <legend> Household Members: </legend>
+        <input type = 'text' value = 'First Name' readonly>
+        <input type = 'text' value = 'Last Name' readonly>
+        <input type = 'text' value = 'Data of Birth' readonly>
+    </fieldset>";
+    while($row = $members_query->fetch_assoc()) {
+        echo "<fieldset>
+            <input type = 'text' value = '" . $row['FirstName'] . "' readonly>
+            <input type = 'text' value = '" . $row['LastName'] . "' readonly>
+            <input type = 'text' value = '" . $row['DateOfBirth'] . "' readonly>
+        </fieldset>";
+    }
+    echo "<br> <br> <input type = 'submit' value = 'Information Correct'>
+    </form>
+    <div class = 'page'>
+        <button onclick = 'displayRegisterPage()'> Information Not Correct </button>
+    </div>";
 }
 else {
-  echo "No records matching your query were found.";
+    echo "<div class = 'page'> 
+        No records matching your query were found <br> <br>
+        <button onclick = 'displayRegisterPage()'> Register Your Information </button>
+    </div>";
 }
 // Close connection
-mysqli_close($connection);
-?>
+mysqli_close($connection); ?>
 <html>
-	<script>
-		function returnRegister() {
-			window.location.href = "../HomePage.php";
-		}
-	</script>
+        <script>
+            function displayRegisterPage() {
+                window.location.href = 'RegisterPage.php';
+            }
+        </script>
+    </body>
 </html>
